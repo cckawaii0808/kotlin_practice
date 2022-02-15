6249d9e82a68450c50e6090a70017cb097ce09f1
cckawaii0808 <a23730800@gmail.com>
2022-02-15 22:17:46 +0800

    備註

---
+++ src/main/kotlin/day1.kt
@@ -0,0 +1,33 @@
+fun main(){
+//命名規則
+
+    println("Hello world")
+    //     var    name :   String = "CC"
+    //    可讀可改  變數名   類型       值
+    var name : String = "CC"
+    name = "Jason XIE "
+
+    var year : Int = 15
+    val weight :Double=52.1
+    println(name+year+"歲 "+weight+" 公斤")
+
+}
+
+
+/*
+String 字符串4
+Char   單字串
+Int
+Double
+List   集合
+Set   五重複的元素集合
+Map    鏈值隊的集合
+ */
+
+
+
+
+
+
+
+
